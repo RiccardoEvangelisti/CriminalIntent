4d2002b5cd805429ff03374c1236852326627680
Riccardo Evangelisti <evangericcardo00@gmail.com>
2022-09-30 15:07:56 +0200

    RecyclerView

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeHolder.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeHolder.kt
@@ -1,8 +1,26 @@
 package com.bignerdranch.android.criminalintent
 
+import android.text.format.DateFormat
+import android.view.View
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bignerdranch.android.criminalintent.databinding.ListItemCrimeBinding
 
 class CrimeHolder(
-    val binding: ListItemCrimeBinding
-) : RecyclerView.ViewHolder(binding.root)
+    private val binding: ListItemCrimeBinding
+) : RecyclerView.ViewHolder(binding.root) {
+    fun bind(crime: Crime) {
+        binding.crimeTitle.text = crime.title
+        binding.crimeDate.text = DateFormat.format("hh:mm a dd/MM/yyyy", crime.date)
+        binding.root.setOnClickListener {
+            Toast.makeText(
+                binding.root.context, "${crime.title} clicked!", Toast.LENGTH_SHORT
+            ).show()
+        }
+        binding.crimeSolved.visibility = if (crime.isSolved) {
+            View.VISIBLE
+        } else {
+            View.GONE
+        }
+    }
+}
--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListAdapter.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListAdapter.kt
@@ -9,10 +9,8 @@ class CrimeListAdapter(
     private val crimes: List<Crime>
 ) : RecyclerView.Adapter<CrimeHolder>() {
 
-
     override fun onCreateViewHolder(
-        parent: ViewGroup,
-        viewType: Int
+        parent: ViewGroup, viewType: Int
     ): CrimeHolder {
         val inflater = LayoutInflater.from(parent.context)
         val binding = ListItemCrimeBinding.inflate(inflater, parent, false)
@@ -21,10 +19,7 @@ class CrimeListAdapter(
 
     override fun onBindViewHolder(holder: CrimeHolder, position: Int) {
         val crime = crimes[position]
-        holder.apply {
-            binding.crimeTitle.text = crime.title
-            binding.crimeDate.text = crime.date.toString()
-        }
+        holder.bind(crime)
     }
 
     override fun getItemCount() = crimes.size
