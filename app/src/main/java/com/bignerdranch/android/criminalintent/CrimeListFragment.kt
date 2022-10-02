acf683041586cd648d9dd5f1cac65d8412837d6d
RiccardoEvangelisti <evangericcardo00@gmail.com>
2022-10-02 18:21:32 +0200

    Capitolo 12

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.kt
@@ -1,16 +1,17 @@
 package com.bignerdranch.android.criminalintent
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.bignerdranch.android.criminalintent.databinding.FragmentCrimeListBinding
-
-private const val TAG = "CrimeListFragment"
+import kotlinx.coroutines.launch
 
 class CrimeListFragment : Fragment() {
 
@@ -22,11 +23,6 @@ class CrimeListFragment : Fragment() {
 
     private val crimeListViewModel: CrimeListViewModel by viewModels()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        Log.d(TAG, "Total crimes: ${crimeListViewModel.crimes.size}")
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -35,12 +31,23 @@ class CrimeListFragment : Fragment() {
         _binding = FragmentCrimeListBinding.inflate(inflater, container, false)
 
         binding.crimeRecyclerView.layoutManager = LinearLayoutManager(context)
-
-        val crimes = crimeListViewModel.crimes
-        val adapter = CrimeListAdapter(crimes)
-        binding.crimeRecyclerView.adapter = adapter
-
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // Eseguo il prelievo dei dati in un viewLifecycleScope, il quale "cancella il lavoro" se la view viene distrutta
+        // (ossia blocca ed elimina l'esecuzione del blocco di codice se questo è ancora in corso
+        viewLifecycleOwner.lifecycleScope.launch {
+            // Ripete il prelievo dei dati (=esegue una coroutine) ogni volta che la view entra nello stato Started,
+            // quindi rimane in esecuzione se è nello stato Resumed, ma viene "cancellata" se torna nello stato Created
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                // Osserva il Flow dei dati. La lambda viene chiamata ogni volta che il dato cambia
+                crimeListViewModel.crimes.collect { crimes ->
+                    binding.crimeRecyclerView.adapter = CrimeListAdapter(crimes)
+                }
+            }
+        }
     }
 
     override fun onDestroyView() {
